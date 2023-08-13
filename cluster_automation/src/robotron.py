b82e0c03ace92283aca1d16050fee94e27ee1c2c
kaesemann <kaesemann@jonthe.net>
2023-08-13 17:59:51 +0200

    changed rsync, removed offline helper, made linter happy

--- cluster_automation/src/robotron.py
+++ cluster_automation/src/robotron.py
@@ -1,29 +1,18 @@
-OFFLINE = True
+""" Automation script for running and retrieving work on the Uni computing cluster.
+
+The script needs to run on a machine with ssh access to the cluster (through VPN).
+Takes a list with an arbitrary number of tuples (target_folder, macfile) to be 
+processed sequentially.
+"""
 
 import time
 import subprocess
-if not OFFLINE:
-    import subprocess
-else:
-    from subprocess import CompletedProcess
-    class subprocess:
-        def __init__(self) -> None:
-            pass
-        def run(commandline, stdout, stderr, universal_newlines, check, timeout):
-            proc = CompletedProcess("", 0, stdout="JOBID", stderr="")
-            print(' '.join(commandline))
-            return proc
 from subprocess import PIPE, STDOUT
 import argparse
 import shlex
 
 def main(worklist):
-    """ Automation script for running and retrieving work on the Uni computing cluster.
-
-    The script needs to run on a machine with ssh access to the cluster (through VPN).
-    Takes a list with an arbitrary number of tuples (target_folder, macfile) to be 
-    processed sequentially.
-    """
+    """ Main """
     print("\nRobotron awakes! Found these tasks:")
     for (target_folder, mac_file) in worklist:
         print(f"Target: {target_folder : <26} Macro: {mac_file}")
@@ -41,22 +30,21 @@ def main(worklist):
         download_from_remote(target_folder)
 
 
-def debug(fn):
+def debug(f_n):
     """ Debug decorator."""
-    if True:
-        def wrapper(*args, **kwargs):
-            print(f"Invoking {fn.__name__}")
-            print(f"  args: {args}")
-            print(f"  kwargs: {kwargs}")
-            result = fn(*args, **kwargs)
-            print(f"  returned {result}")
-            return result
-        return wrapper
+    def wrapper(*args, **kwargs):
+        print(f"Invoking {f_n.__name__}")
+        print(f"  args: {args}")
+        print(f"  kwargs: {kwargs}")
+        result = f_n(*args, **kwargs)
+        print(f"  returned {result}")
+        return result
+    return wrapper
 
 
-def wait_for_numbercrunching(timeout=(24 * 60 * 60)):
+def wait_for_numbercrunching(timeout=24 * 60 * 60):
     """ Sleeps until the queue has finished. Raises TimeoutError on timeout."""
-    poll_interval = 1       # in seconds # TODO: slow down adequately
+    poll_interval = 1       # in seconds .. slow down adequately
     while not my_queue_isempty():
         timeout -= poll_interval
         if timeout > 0:
@@ -69,12 +57,12 @@ def wait_for_numbercrunching(timeout=(24 * 60 * 60)):
 def my_queue_isempty():
     """ Parses the queue status to check if the queue is finished."""
     queue_status = get_queue_status()
-    (USER, _) = get_user_host()
+    (user, _) = get_user_host()
 
     # As long as our username appears in the queue we assume our work is not done yet.
     # This check could be improved by looking for specific job IDs for cases
-    # when the user may be running other work from outside this script instance.    
-    if USER in queue_status:
+    # when the user may be running other work from outside this script instance.
+    if user in queue_status:
         empty = False
     # Test that at least some expected string was returned (e.g. table header)
     elif "JOBID" in queue_status:
@@ -90,7 +78,7 @@ def get_queue_status():
     commandline = wrap_in_ssh(command)
 
     try:
-        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT, 
+        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT,
                                 universal_newlines=True, check=True, timeout=15)
     except subprocess.CalledProcessError as err:
         # Process ran but returned non-zero. If excepted, handle here.
@@ -102,11 +90,10 @@ def get_queue_status():
 
 def wrap_in_ssh(commandline):
     """ Takes a command line and wraps it to be executed remotely through SSH."""
-    (USER, HOST) = get_user_host()
-    user_host = f"{USER}@{HOST}"
-
-    cmdline = f"/usr/bin/sshpass -v -f /home/cris/.ssh/ovgu-cluster-pass /usr/bin/ssh -v {user_host} '{commandline}'"
-    return shlex.split(cmdline)
+    (user, host) = get_user_host()
+    sshp = "/usr/bin/sshpass -f /home/cris/.ssh/ovgu-cluster-pass"
+    sshc = f"/usr/bin/ssh {user}@{host} '{commandline}'"
+    return shlex.split(f"{sshp} {sshc}")
 
 
 def upload_to_remote():
@@ -116,15 +103,16 @@ def upload_to_remote():
 
 def download_from_remote(target_folder):
     """ Downloads the work results from the cluster. """
-    (USER, HOST) = get_user_host()
-    BASE_PATH = "/beegfs2/scratch/"
-    # FIXME: Use shlex!
-    source = f"{USER}@{HOST}:{BASE_PATH}{USER}/JOB/{target_folder}/output/"
+    (user, host) = get_user_host()
+    base_path = "/beegfs2/scratch/"
+    source = f"{user}@{host}:{base_path}{user}/JOB/{target_folder}/output/"
     target = f"/home/cris/nextcloudshare/Simulations/{target_folder}/output/"
-    commandline = ["/usr/bin/sshpass -f /home/cris/.ssh/ovgu-cluster-pass rsync", "-av", "-e ssh", "--include '*/'", "--include='*.dat'", "--exclude='*'", source, target]
+    sshp = "/usr/bin/sshpass -f /home/cris/.ssh/ovgu-cluster-pass"
+    rsyn = f"rsync -av -e ssh --include '*/' --include='*.dat' --exclude='*' {source} {target}"
+    commandline = shlex.split(f"{sshp} {rsyn}")
 
     try:
-        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT, 
+        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT,
                                 universal_newlines=True, check=True, timeout=15)
     except subprocess.CalledProcessError as err:
         # Process ran but returned non-zero. If excepted, handle here.
@@ -140,11 +128,12 @@ def prepare_work():
 
 def start_numbercrunching(target_folder, mac_file):
     """ Runs the simulation on the cluster. """
-    command = f"/home/chifu/scratch/tmp/hemispheric_PET/run_Simulation.sh {target_folder} {mac_file}"
+    script = "/home/chifu/scratch/tmp/hemispheric_PET/run_Simulation.sh"
+    command = f"{script} {target_folder} {mac_file}"
     commandline = wrap_in_ssh(command)
 
     try:
-        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT, 
+        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT,
                                 universal_newlines=True, check=True, timeout=15)
     except subprocess.CalledProcessError as err:
         # Process ran but returned non-zero. If excepted, handle here.
@@ -159,7 +148,7 @@ def finalize_work(target_folder, root_file_name):
     commandline = wrap_in_ssh(command)
 
     try:
-        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT, 
+        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT,
                                 universal_newlines=True, check=True, timeout=15)
     except subprocess.CalledProcessError as err:
         # Process ran but returned non-zero. If excepted, handle here.
@@ -181,13 +170,13 @@ if __name__ == "__main__":
                         nargs='+', 
                         metavar='target_folder mac_file', 
                         help='one or more pairs of (target folder, mac file) seperated by a space')
-    args = parser.parse_args()
-    
-    if len(args.targetfolder_macfile) % 2:
+    arguments = parser.parse_args()
+
+    if len(arguments.targetfolder_macfile) % 2:
         parser.error('Found odd number of arguments. Expected pairs of target/macfile.')
     else:
-        worklist = []
-        def dualiter(singleiter):
+        wrklist = []
+        def _dualiter(singleiter):
             iterable = iter(singleiter)
             while True:
                 try:
@@ -195,7 +184,7 @@ if __name__ == "__main__":
                 except StopIteration:
                     break
 
-        for target_folder, mac_file in dualiter(args.targetfolder_macfile):
-            worklist.append((target_folder, mac_file))
+        for trgt_fldr, mc_file in _dualiter(arguments.targetfolder_macfile):
+            wrklist.append((trgt_fldr, mc_file))
 
-    main(worklist)
+    main(wrklist)
