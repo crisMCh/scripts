d0e569322579ed19814f98e8206a4cfb60dbbcd3
kaesemann <kaesemann@jonthe.net>
2023-08-14 14:14:11 +0200

    implemented upload_sh.py

---
+++ cluster_automation/src/upload_sh.py
@@ -0,0 +1,36 @@
+""" Uploads the shell scripts used by robotron. """
+
+import subprocess
+from subprocess import PIPE, STDOUT
+from os import path
+
+import shlex
+
+from robotron import get_user_host
+
+
+def upload():
+    """ Upload """
+    (user, host) = get_user_host()
+    source = path.join('..', 'sh', '*')
+    remote = f"/home/{user}/scratch/tmp/hemispheric_PET/"
+    target = f"{user}@{host}:{remote}"
+    sshp = "/usr/bin/sshpass -f /home/cris/.ssh/ovgu-cluster-pass"
+    rsyn = f"rsync -av -e ssh {source} {target}"
+    commandline = f"{sshp} {rsyn}"
+    #print(commandline)
+    commandline = shlex.split(commandline)
+    #print(commandline)
+
+    try:
+        result = subprocess.run(commandline, stdout=PIPE, stderr=STDOUT,
+                                universal_newlines=True, check=True, timeout=15)
+    except subprocess.CalledProcessError as err:
+        # Process ran but returned non-zero. If excepted, handle here.
+        print(err.returncode)
+        raise
+    return result.stdout
+
+
+if __name__ == "__main__":
+    print(upload())
